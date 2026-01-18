f7d257ac728bd01fc41c20335222d925a8f5bb3d
NarakCODE <channarakluy@gmail.com>
2026-01-18 13:24:28 +0700

    change the static port

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 8081,
   mongoUri:
     process.env.MONGODB_URI ||
     process.env.DATABASE_URL ||
